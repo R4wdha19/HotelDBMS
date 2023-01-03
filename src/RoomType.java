68f9c510877c6d0c717ab95af0add98df3b539e2
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-03 17:01:02 +0400

    Last update

--- src/RoomType.java
+++ src/RoomType.java
@@ -139,4 +139,169 @@ public class RoomType {
 
 	}
 
+	public static void getById() {
+		System.out.println(" Please Enter The ID Of The Row To Print");
+		Scanner sc1 = new Scanner(System.in);
+		int userInput = sc1.nextInt();
+
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+
+		String user = "sa";
+		String pass = "root";
+		Connection con = null;
+		String sqlQueryToRead = "SELECT * FROM Room_Type WHERE id =" + userInput;
+		try {
+
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+
+			DriverManager.registerDriver(driver);
+
+			con = DriverManager.getConnection(url, user, pass);
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlQueryToRead);
+			int count = 0;
+			while (resultSet.next() && count <= userInput) {
+				Integer id = resultSet.getInt("id");
+				String roomTypeName = resultSet.getString("room_type_name");
+				Date createdDate = resultSet.getDate("created_date");
+				Date updatedDate = resultSet.getDate("updated_date");
+				Boolean isActive = resultSet.getBoolean("is_Active");
+				System.out.println(id + " " + roomTypeName + " " + createdDate + " " + updatedDate + " " + isActive);
+				count++;
+			}
+
+			con.close();
+		}
+
+		catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
+	public static void updateById() {
+		System.out.println(" Please Enter The ID To Updat Its Data");
+		Scanner sc1 = new Scanner(System.in);
+		int userInput = sc1.nextInt();
+		System.out.println(" Please Enter The New Room Type Name");
+		String roomTypeName = sc1.next();
+		Date date = new Date(System.currentTimeMillis());
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+
+		String user = "sa";
+		String pass = "root";
+		Connection con = null;
+		String sqlQueryToUpdate = "UPDATE Room_Type SET room_type_name = " + "'" + roomTypeName + "',"
+				+ " updated_date = " + "'" + date + "' WHERE id =" + userInput;
+		System.out.println(sqlQueryToUpdate);
+		try {
+
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+
+			DriverManager.registerDriver(driver);
+
+			con = DriverManager.getConnection(url, user, pass);
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlQueryToUpdate);
+			System.out.println(resultSet);
+			int count = 0;
+			while (resultSet.next() && count <= userInput) {
+				Integer id = resultSet.getInt("id");
+				String roomType_Name = resultSet.getString("room_type_name");
+				Date createdDate = resultSet.getDate("created_date");
+				Date updatedDate = resultSet.getDate("updated_date");
+				Boolean isActive = resultSet.getBoolean("is_Active");
+				System.out.println(id + " " + roomType_Name + " " + createdDate + " " + updatedDate + " " + isActive);
+				count++;
+			}
+			con.close();
+		}
+
+		catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+	}
+
+	public static void deleteById() {
+		System.out.println(" Please Enter The ID To Delete The Row");
+		Scanner sc1 = new Scanner(System.in);
+		int userInput = sc1.nextInt();
+
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+
+		String user = "sa";
+		String pass = "root";
+		Connection con = null;
+		String sqlQueryToRead = "DELETE FROM Room_Type WHERE id =" + userInput;
+		try {
+
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+
+			DriverManager.registerDriver(driver);
+
+			con = DriverManager.getConnection(url, user, pass);
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlQueryToRead);
+			int count = 0;
+			while (resultSet.next() && count <= userInput) {
+				Integer id = resultSet.getInt("id");
+				String roomTypeName = resultSet.getString("room_type_name");
+				Date createdDate = resultSet.getDate("created_date");
+				Date updatedDate = resultSet.getDate("updated_date");
+				Boolean isActive = resultSet.getBoolean("is_Active");
+				System.out.println(id + " " + roomTypeName + " " + createdDate + " " + updatedDate + " " + isActive);
+				count++;
+			}
+			con.close();
+		}
+
+		catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
+	public static void updateIsActive() {
+		System.out.println(" Please Enter The Number Of Rows  To Updat Its Status");
+		Scanner sc1 = new Scanner(System.in);
+		int userInput = sc1.nextInt();
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+
+		String user = "sa";
+		String pass = "root";
+		Connection con = null;
+		String sqlQueryToUpdate = "UPDATE Room_Type SET is_active = 0 where id ='" + userInput + "'";
+		System.out.println(sqlQueryToUpdate);
+		try {
+
+			Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+
+			DriverManager.registerDriver(driver);
+
+			con = DriverManager.getConnection(url, user, pass);
+			Statement statement = con.createStatement();
+			ResultSet resultSet = statement.executeQuery(sqlQueryToUpdate);
+			System.out.println(resultSet);
+			int count = 0;
+			while (resultSet.next() && count <= userInput) {
+				Integer id = resultSet.getInt("id");
+				String roomTypeName = resultSet.getString("room_type_name");
+				Date createdDate = resultSet.getDate("created_date");
+				Date updatedDate = resultSet.getDate("updated_date");
+				Boolean isActive = resultSet.getBoolean("is_Active");
+				System.out.println(id + " " + roomTypeName + " " + createdDate + " " + updatedDate + " " + isActive);
+				count++;
+			}
+			con.close();
+		}
+
+		catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+	}
+
 }
