8169a9875f1d2212acc6b41d070abd4c10551de1
R4wdha19 <116624370+R4wdha19@users.noreply.github.com>
2023-01-16 15:11:43 +0400

    Updated Managment

--- src/HotelManagement.java
+++ src/HotelManagement.java
@@ -3,13 +3,11 @@ package src;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HotelManagement {
 
 	public static final String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
-	// static variables to be used from static function so it stays in static
-	// context
-//   " final " it can not change or update
 	public static final String user = "sa";
 	public static final String pass = "root";
 	public static Connection con;
@@ -26,11 +24,121 @@ public class HotelManagement {
 
 	public static void GuestNameEndWithLetter() {
 
+		String nameEndWithE = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(nameEndWithE);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + nameEndWithE);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
 	}
-//	SELECT * WHERE Guest_name Like %E%;
+
 //	Rooms where guests are paying more than 1000
-//	Count of guests who are staing in 'DELUXE' rooms
-//	Guests who are staing in rooms and served by employee who have 'A' in their name
+	public static void GuestpayingMoreThan1000() {
+
+		String GuestpayingMoreThan1000 = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(GuestpayingMoreThan1000);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + GuestpayingMoreThan1000);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
+//	Count of guests who are staying in 'DELUXE' rooms
+	public static void GuestStayingInSpecificRoomType() {
+
+		String GuestpayingMoreThan1000 = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(GuestpayingMoreThan1000);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + GuestpayingMoreThan1000);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
+//	Guests who are staying in rooms and served by employee who have 'A' in their name
+	public static void GuestServedByEmployee() {
+
+		String GuestpayingMoreThan1000 = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(GuestpayingMoreThan1000);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + GuestpayingMoreThan1000);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
 //	All rooms which are not active but room type is 'Deluxe'
+	public static void RoomsIsActiveStatusAndSpecificRoomType() {
+
+		String GuestpayingMoreThan1000 = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(GuestpayingMoreThan1000);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + GuestpayingMoreThan1000);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
+
 //	All room type in hotels who's name have 'H' or are active but have more than 5 rooms.
+
+	public static void hotelsNameWithSpecificRoomNumber() {
+
+		String GuestpayingMoreThan1000 = " SELECT * FROM GUESTS WHERE guest_name LIKE 'E%'";
+
+		try {
+			Statement st = con.createStatement();
+			int executing = st.executeUpdate(GuestpayingMoreThan1000);
+			if (executing >= 0) {
+				System.out.println("Created Successfully : " + GuestpayingMoreThan1000);
+			} else {
+				System.out.println("Creation Is Failed");
+			}
+		} catch (Exception ex) {
+
+			System.err.println(ex);
+		}
+
+	}
 }
